1a89852fb8df260bce62d34c83a95260ac100146
cmsbuild <>
2009-11-15 12:32:54 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- EcalEndcapMonitorTasks-V00-14-01'.
    Sprout from master 2009-11-09 10:19:49 UTC Emanuele Di Marco <sha1-7725c4b6a787836a48e2e74f4d20b6a79f483406@cern.ch> 'move the occupancy projections from TH1F -> TH1D to avoid saturation during long runs'
    Cherrypick from master 2009-11-15 12:32:53 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> '':
        src/EESelectiveReadoutTask.cc

--- src/EESelectiveReadoutTask.cc
+++ src/EESelectiveReadoutTask.cc
@@ -1,8 +1,8 @@
 /*
  * \file EESelectiveReadoutTask.cc
  *
- * $Date: 2009/10/13 11:41:18 $
- * $Revision: 1.37 $
+ * $Date: 2009/11/15 12:23:23 $
+ * $Revision: 1.41 $
  * \author P. Gras
  * \author E. Di Marco
  *
@@ -123,6 +123,12 @@ void EESelectiveReadoutTask::beginRun(const Run& r, const EventSetup& c) {
         nEvtFullReadout[ix][iy][iz] = 0;
         nEvtRUForced[ix][iy][iz] = 0;
         nEvtAnyReadout[ix][iy][iz] = 0;
+      }
+    }
+  }
+  for(int ix = 0; ix < 100; ix++ ) {
+    for(int iy = 0; iy < 100; iy++ ) {
+      for(int iz = 0; iz < 2; iz++) {
         nEvtHighInterest[ix][iy][iz] = 0;
         nEvtLowInterest[ix][iy][iz] = 0;
         nEvtAnyInterest[ix][iy][iz] = 0;
@@ -470,9 +476,9 @@ void EESelectiveReadoutTask::analyze(const Event& e, const EventSetup& c){
     LogWarning("EESelectiveReadoutTask") << EESRFlagCollection_ << " not available";
   }
 
-  for(int iz = 0; iz < 2; iz++) {
-    for(int ix = 0; ix < 20; ix++ ) {
-      for(int iy = 0; iy < 20; iy++ ) {
+  for(int ix = 0; ix < 20; ix++ ) {
+    for(int iy = 0; iy < 20; iy++ ) {
+      for(int iz = 0; iz < 2; iz++) {
 
         if( nEvtAnyReadout[ix][iy][iz] ) {
 
@@ -566,10 +572,9 @@ void EESelectiveReadoutTask::analyze(const Event& e, const EventSetup& c){
           double towerSize =  nCrySC[ix][iy][iz] * bytesPerCrystal;
 
           float xix = ix;
-
           if ( iz == 0 ) xix = 19 - xix;
-
           xix += 0.5;
+
           float xiy = iy+0.5;
 
           EETowerSize_[iz]->Fill(xix, xiy, towerSize);
@@ -630,14 +635,15 @@ void EESelectiveReadoutTask::analyze(const Event& e, const EventSetup& c){
     LogWarning("EESelectiveReadoutTask") << EcalTrigPrimDigiCollection_ << " not available";
   }
 
-  for(int iz = 0; iz < 2; iz++) {
-    for(int ix = 0; ix < 100; ix++ ) {
-      for(int iy = 0; iy < 100; iy++ ) {
+  for(int ix = 0; ix < 100; ix++ ) {
+    for(int iy = 0; iy < 100; iy++ ) {
+      for(int iz = 0; iz < 2; iz++) {
 
         if( nEvtAnyInterest[ix][iy][iz] ) {
 
           float xix = ix-0.5;
           if ( iz == 0 ) xix = 100 - xix;
+
           float xiy = iy-0.5;
 
           float fraction = float(nEvtHighInterest[ix][iy][iz] / nEvtAnyInterest[ix][iy][iz]);
@@ -654,7 +660,6 @@ void EESelectiveReadoutTask::analyze(const Event& e, const EventSetup& c){
 
           EEHighInterestTriggerTowerFlagMap_[iz]->setBinContent(binx, biny, fraction);
           EEHighInterestTriggerTowerFlagMap_[iz]->setBinError(binx, biny, error);
-
 
           fraction = float(nEvtLowInterest[ix][iy][iz] / nEvtAnyInterest[ix][iy][iz]);
           error = sqrt(fraction*(1-fraction)/float(nEvtAnyInterest[ix][iy][iz]));
