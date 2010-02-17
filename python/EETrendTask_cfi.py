c24967044b3e910a31c34ab4fc486b074f15d0b7
cmsbuild <>
2010-02-17 22:46:53 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- EcalEndcapMonitorTasks-CMSSW_3_6_0_pre1'.
    Sprout from master 2010-02-17 22:46:52 UTC W.David Dagenhart <sha1-9ca965af9cf1904746014c29a94c3544e5eaff4b@cern.ch> 'Central migration of TriggerNames class interface'
    Cherrypick from master 2009-12-14 16:50:54 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> '':
        interface/EETrendTask.h
        python/EETrendTask_cfi.py
        src/EETrendTask.cc

--- python/EETrendTask_cfi.py
+++ python/EETrendTask_cfi.py
@@ -4,7 +4,6 @@ ecalEndcapTrendTask = cms.EDAnalyzer("EETrendTask",
     prefixME = cms.untracked.string('EcalEndcap'),
     enableCleanup = cms.untracked.bool(False),
     mergeRuns = cms.untracked.bool(False),
-    verbose = cms.untracked.bool(False),
     EEDigiCollection = cms.InputTag("ecalEBunpacker","eeDigis"),
     EcalRecHitCollection = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
     FEDRawDataCollection = cms.InputTag("source")
